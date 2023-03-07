308feafb0b6f004ec8222c6114b359fb7d6ff9a2
Project <kshamata@p.com>
2023-03-07 15:53:15 +0545

    update

--- src/Payment.js
+++ src/Payment.js
@@ -2,28 +2,38 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import HeroSection from './components/HeroSection/HeroSection';
 import './Payment.scss';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Payment = () => {
     const data = {
         name: "Buy you product",
         image: "./images/bg7.jpg",
       };
+      const diffToast = () => {
+        toast("Payment Successful!",
+        {position: "top-center",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",});
+        
+      }
   return (
     <>
       <HeroSection {...data}/>
       <div className='payment__wrapper'>
         <div className='payment__section'>
-          {/* <div className='payment__cod'>
-              place order for cash on delivery
-              Cash on Delivery
-          </div> */}
           <div className='payment__card'>
               {/* Paywith card */}
               <div className='card'>
                 <h2>Enter your payment details</h2>
                 <div className='card__box'>
                 <form>
-                  <input type='number' placeholder='Card Number' required />
+                  <input type='text' placeholder='Card Number' required />
                   <input type='date' placeholder='MM/YY CVC' required />
                 </form>
                 </div>
@@ -43,8 +53,22 @@ const Payment = () => {
                 </div>
             </div>
         </div>
-        <NavLink to='/productsGallery'><button className='pay'>Pay</button></NavLink>
+        {/* <NavLink to='/productsGallery'> */}
+          <button className='pay' onClick={diffToast}>Pay</button>
+          {/* </NavLink> */}
       </div>
+      <ToastContainer 
+      position="top-center"
+      autoClose={1000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+      />
     </>
   )
 }
